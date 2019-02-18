e2cc1a664d99c5e3f9dbefeff8817163f211ea13
EugeneTeam <41008632+EugeneTeam@users.noreply.github.com>
2019-02-18 13:08:23 +0200

    new routing paths

--- node server/index.js
+++ node server/index.js
@@ -3,5 +3,15 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req,res) => {
-    res.send("Hello test")
-}).listen(port);
+    res.send("main")
+});
+
+app.get('/category', (req,res) => {
+    res.send("category")
+});
+
+app.get('/article', (req,res) => {
+    res.send("article")
+});
+
+app.listen(port);
