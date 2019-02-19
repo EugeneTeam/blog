37dbd7867ec4409db898d938c99915e8daed7926
EugeneTeam <41008632+EugeneTeam@users.noreply.github.com>
2019-02-19 20:22:51 +0200

    test

--- node server/index.js
+++ node server/index.js
@@ -12,16 +12,7 @@ app.get('/category', (req,res) => {
 });
 
 app.get('/article', (req,res) => {
-    /*res.send(db.article.findAll({
-        where: {
-            id: 1
-        }
-    }));*/
-    db.article.findAll({
-         where: { id: 1}}).then(projects => {
-        res.send(projects);
-      });
-    //query
+    
     console.log('--------------------------------------------------------');
 });
 
